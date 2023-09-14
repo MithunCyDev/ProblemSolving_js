77a7af21594cab264421e85ae6e3b580e4a6b6f8
Mithuncy <58121761+Mithuncy01@users.noreply.github.com>
2023-09-14 15:14:10 +0600

    Update index.js

--- index.js
+++ index.js
@@ -523,3 +523,21 @@ console.log(area); // Output: 9
 
 const result = climbingLeaderboard(ranked, player);
 console.log(result);
+
+//utopianTree
+function utopianTree(n) {
+    let height = 1;
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 !== 0) {
+            height *= 2;
+        } else {
+            height += 1;
+        }
+    }
+    return height;
+}
+
+// Example usage:
+console.log(utopianTree(0)); // Output: 1
+console.log(utopianTree(1)); // Output: 2
+console.log(utopianTree(4)); // Output: 7
