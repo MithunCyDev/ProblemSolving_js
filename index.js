822455de2364b82ddd26154f9bffdfebe8effa50
Mithuncy <58121761+Mithuncy01@users.noreply.github.com>
2023-09-21 22:06:55 +0600

    Update index.js

--- index.js
+++ index.js
@@ -722,3 +722,17 @@ for (let testCase of testCases) {
     console.log(typeof testCase)
 }
 
+//extraLongFactorials
+function extraLongFactorials(n) {
+    let result = BigInt(1);
+    for (let i = 2n; i <= BigInt(n); i++) {
+        result *= i;
+        console.log(i)
+    }
+
+    console.log(result.toString());
+}
+
+// Example usage:
+extraLongFactorials(25);
+
