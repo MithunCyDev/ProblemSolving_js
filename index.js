02f5bf622813e1594cf818c5c770441f594c80ed
Mithuncy <58121761+Mithuncy01@users.noreply.github.com>
2023-10-08 02:32:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1053,3 +1053,38 @@ function encryption(s) {
 
   const result = encryption("Hello")
   console.log(result)
+
+
+//biggerIsGreater
+function biggerIsGreater(w) {
+    w = w.split('')
+    // Find non-increasing suffix
+    let i = w.length - 1;
+    while (i > 0 && w[i - 1] >= w[i])
+        i--;
+
+    if (i <= 0)
+        return 'no answer';
+    
+    // Find the rightmost successor to pivot in the suffix
+    let j = w.length - 1;
+    while (w[j] <= w[i - 1])
+        j--;
+
+    // Swap the pivot with the rightmost character
+    const temp = w[i - 1];
+    w[i - 1] = w[j];
+    w[j] = temp;
+    
+    // Reverse suffix
+    j = w.length - 1;
+    while (i < j) {
+        const temp = w[i];
+        w[i] = w[j];
+        w[j] = temp;
+        i++;
+        j--;
+    }
+
+    return w.join('');
+}
